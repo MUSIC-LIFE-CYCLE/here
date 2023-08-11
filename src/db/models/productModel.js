8a13c685562bfafe784119d99c1798e4463ad2bb
donggram2 <113027954+donggram2@users.noreply.github.com>
2023-08-11 23:47:05 +0900

    다섯 번째의 커밋

--- src/db/models/productModel.js
+++ src/db/models/productModel.js
@@ -2,44 +2,53 @@ import { productSchema } from "../schemas/product";
 import { model } from "mongoose";
 
 const Product = model("product", productSchema);
-
+// "product" 모델 생성
 class productModel {
+  // 새로운 상품 생성 메서드
   static async create(newProduct) {
     try {
       return await Product.create(newProduct);
     } catch (error) {
+      // 오류 발생 시 실패 메시지 반환
       return { errorMessage: "제품 등록에 실패했습니다." };
     }
   }
-
+  // 상품 이름으로 조회 메서드
   static async findByName(name) {
     try {
       return await Product.findOne({ name });
     } catch (error) {
-      return null;
+      // 오류 발생 시 실패 메시지 반환
+      return { errorMessage: "제품 조회에 실패했습니다." };
     }
   }
 
+  // 상품 정보 부분 업데이트 메서드
   static async patchProduct(productId, patchData) {
     try {
       return await Product.findByIdAndUpdate(productId, patchData);
     } catch (error) {
+      // 오류 발생 시 실패 메시지 반환
       return { errorMessage: "제품 부분 업데이트에 실패하였습니다." };
     }
   }
 
+  // 모든 상품 조회 메서드
   static async getProducts() {
     try {
       return await Product.find({});
     } catch (error) {
+      // 오류 발생 시 실패 메시지 반환
       return { errorMessage: "제품 조회에 실패하였습니다." };
     }
   }
 
+  // 상품 삭제 메서드
   static async deleteProduct(productId) {
     try {
       return await Product.deleteOne({ _id: productId });
     } catch (error) {
+      // 오류 발생 시 실패 메시지 반환
       return { errorMessage: "제품 삭제에 실패하였습니다." };
     }
   }
--- src/services/productService.js
+++ src/services/productService.js
@@ -5,47 +5,62 @@ class productService {
   static async addProduct({ name, price, description }) {
     // 여기에 코드 작성
     try {
+      // 상품 등록 메서드
       const checkProduct = await productModel.findByName(name);
       if (checkProduct) {
-        return { errorMessage: "이미 등록된 상품입니다." };
+        return { errorMessage: "이미 등록된 제품입니다." };
       }
+
+      // 새로운 상품 생성
       const createNewProduct = await productModel.create({
         id: uuidv4(),
         name: name,
         price: price,
         description: description,
       });
-      createNewProduct.errorMessage = null;
 
+      // 에러 메시지 초기화 후 생성된 상품 반환
+      createNewProduct.errorMessage = null;
       return createNewProduct;
     } catch (error) {
-      return { errorMessage: "상품 등록에 실패하였습니다." };
+      // 오류 발생 시 실패 메시지 반환
+      return { errorMessage: "제품 등록에 실패하였습니다." };
     }
   }
+
+  // 상품 정보 업데이트 메서드
   static async patchProduct(productId, patchData) {
     try {
+      // 상품 정보 부분 업데이트
       const patchProduct = await productModel.patchProduct(
         productId,
         patchData
       );
       return patchProduct;
     } catch (error) {
+      // 오류 발생 시 실패 메시지 반환
       return { errorMessage: "제품 부분 업데이트에 실패하였습니다." };
     }
   }
+
+  // 상품 목록 조회 메서드
   static async getProducts() {
     try {
       const product = await productModel.getProducts({});
       return product;
     } catch (error) {
+      // 오류 발생 시 실패 메시지 반환
       return { errorMessage: "제품 조회에 실패하였습니다." };
     }
   }
+
+  // 상품 삭제 메서드
   static async deleteProduct(productId) {
     try {
       const product = await productModel.deleteProduct(productId);
       return product;
     } catch (error) {
+      // 오류 발생 시 실패 메시지 반환
       return { errorMessage: "제품 삭제에 실패하였습니다." };
     }
   }
